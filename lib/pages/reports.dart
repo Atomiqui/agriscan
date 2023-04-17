9fbca532a3b71136bf5bb47979ba697c652334a2
Alisson Costa Schmidt <acschmidt@inf.ufsm.br>
2023-04-16 23:48:46 -0300

    criadoo cabeçalho dos reports.

--- lib/pages/reports.dart
+++ lib/pages/reports.dart
@@ -15,6 +15,63 @@ class _ReportsState extends State<Reports> {
         title: const Text('Reports'),
         actions: const [],
       ),
+      body: ListView.builder(
+        itemCount: 5, // Lista_de_lavouras.length
+        itemBuilder: (BuildContext context, int index) {
+          return const Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/aplicacoes.jpg'),
+                    radius: 20.0, // ajuste este valor para alterar o tamanho da imagem
+                  ),
+                  SizedBox(width: 10.0), // espaço entre a imagem e o próximo widget no row
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Lavoura do pai do Gelso',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Arial',
+                          //fontStyle: FontStyle.italic,
+                          //decoration: TextDecoration.underline,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        'Última modificação em 10/03/2022',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                          //fontWeight: FontWeight.bold,
+                          fontFamily: 'Arial',
+                          //fontStyle: FontStyle.italic,
+                          //decoration: TextDecoration.underline,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  IconButton(
+                    icon: Icon(Icons.more_horiz),
+                    onPressed: null,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  
+                ],
+              ),
+            ],
+          );
+        }
+      )
     );
   }
 }
